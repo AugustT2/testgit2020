3adfe217f83028c8baaf1dcf8f52223160faffd5
Chen <Frank.Chen@nike.com>
2020-06-08 11:30:21 +0800

    Initial commit

---
+++ testgit2020/src/main/java/com/chenhuan/testgit2020/Testgit2020Application.java
@@ -0,0 +1,13 @@
+package com.chenhuan.testgit2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testgit2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testgit2020Application.class, args);
+    }
+
+}
